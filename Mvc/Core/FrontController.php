16bf38e8568c3e77e30d2660c70af040ab2d09cd
Petar Petkov <petar.b.petkov@gmail.com>
2015-10-03 08:14:56 +0300

    Custom routes improvement

--- Mvc/Core/FrontController.php
+++ Mvc/Core/FrontController.php
@@ -1,120 +1,187 @@
 <?php
 
-namespace My\Mvc\Core {
+namespace My\Mvc\Mvc;
 
+use My\Mvc\Interfaces\IRouter;
+use DH\ShoppingCart\Models\BindingModels\User\LoginUser;
 
-    class FrontController {
+class FrontController
+{
+    private static $_instance = null;
+    private $namespace = null;
+    private $controller = null;
+    private $method = null;
+    private $router = null;
+    /**
+     * @var \My\Mvc\Config\Config
+     */
+    private $config = null;
 
-        private static $_instance = null;
-        private $namespace = null;
-        private $controller = null;
-        private $method = null;
-        /**
-         * @var \My\Mvc\Config\Config
-         */
-        private $config = null;
-        /**
-         * @var \My\Mvc\Interfaces\IRouter;
-         */
-        private $router=null;
+    /**
+     * @return \My\Mvc\Interfaces\IRouter
+     */
+    public function getRouter()
+    {
+        return $this->router;
+    }
 
-        private function __construct()
-        {
-            $this->config = \My\Mvc\Core\Application::getInstance()->getConfig();
+    /**
+     * @param \My\Mvc\Interfaces\IRouter $router
+     */
+    public function setRouter(\My\Mvc\Interfaces\IRouter $router)
+    {
+        $this->router = $router;
+    }
+
+    private function __construct()
+    {
+        $this->config = \My\Mvc\Core\Application::getInstance()->getConfig();
+    }
+
+    public function dispatch()
+    {
+        if($this->router == null) {
+            throw new \Exception('No valid router found.', 500);
+        }
+        $uri = $this->router->getURI();
+        $input = \My\Mvc\Core\InputData::getInstance();
+
+        if($this->parseCustomRoute($input, $uri)){
+            return;
         }
 
-        public function getRouter() {
-            return $this->router;
-        }
-
-        public function setRouter(\My\Mvc\Interfaces\IRouter $router) {
-            $this->router = $router;
-        }
-
-        public function dispatch() {
-            if ($this->router == null) {
-                throw new \Exception('No valid router found',500);
+        $params = explode('/', $uri);
+        $this->controller = array_shift($params);
+        if(!$this->controller) {
+            $this->controller = $this->getDefaultController();
+            $this->method = $this->getDefaultMethod();
+        } else {
+            $this->method = array_shift($params);
+            if(!$this->method) {
+                $this->method = $this->getDefaultMethod();
             }
-            $_uri = $this->router->getURI();
-            $routes = \My\Mvc\Core\Application::getInstance()->getConfig()->routes;
-            $routeController=null;
-            if (is_array($routes) && count($routes) > 0) {
-                foreach ($routes as $k => $v) {
-                    if (stripos($_uri, $k) === 0 && ($_uri == $k || stripos($_uri, $k . '/') === 0) && $v['namespace']) {
-                        $this->namespace = $v['namespace'];
-                        $_uri = substr($_uri, strlen($k) + 1);
-                        $routeController=$v;
-                        break;
+            $input->setGet($params);
+        }
+        $this->namespace = $this->config->app['defaultControllerNamespace'];
+
+        $input->setPost($this->router->getPost());
+        $this->loadControllerAction();
+    }
+
+    public function getDefaultController()
+    {
+        $controller = $this->config->app['default_controller'];
+        if ($controller) {
+            return strtolower($controller);
+        }
+
+        return 'index';
+    }
+
+    public function getDefaultMethod()
+    {
+        $method = $this->config->app['default_method'];
+        if ($method) {
+            return strtolower($method);
+        }
+
+        return 'index';
+    }
+
+    /**
+     * @return \My\Mvc\Core\FrontController
+     */
+    public static function getInstance()
+    {
+        if (self::$_instance == null) {
+            self::$_instance = new  \My\Mvc\Core\FrontController();
+        }
+
+        return self::$_instance;
+    }
+
+    public function loadControllerAction()
+    {
+        $controllerPrefix = $this->controller;
+        $this->controller = $this->controller . 'Controller';
+        $namespaceClass = $this->namespace . '\\' . ucfirst($this->controller);
+
+        $isCallable = false;
+        try {
+            $isCallable = is_callable(array($namespaceClass, $this->method));
+        } catch (\Exception $ex) {
+            throw new \Exception('Not found', 404);
+        }
+
+        if ($isCallable) {
+            $bindingModel = null;
+            if($this->router->getPost()) {
+                $bindingModel = BindingModel::validate($this->router->getPost(),
+                    $this->config->bindingModels[$this->namespace][$controllerPrefix][$this->method]);
+            }
+
+            $newController = new $namespaceClass();
+            call_user_func(array($newController, $this->method), $bindingModel);
+        } else {
+            throw new \Exception('Not found', 404);
+        }
+    }
+
+    public function parseCustomRoute($input, $uri)
+    {
+        $customRoutes = $this->config->routes;
+        if($customRoutes && is_array($customRoutes)) {
+            foreach($customRoutes as $customRoute => $controllerAction) {
+                $routeParts = explode('/{', $customRoute);
+                $matchedCustomRoute = array_shift($routeParts);
+                $hasParams = preg_match_all('/{(.+?)}/i', $customRoute, $result);
+                $routeParamNames = $result[1];
+
+                if(stripos($uri, $matchedCustomRoute) === 0
+                    && ($uri == $matchedCustomRoute || stripos($uri, $matchedCustomRoute . '/') === 0)) {
+
+                    if($hasParams) {
+                        $paramsUri = substr($uri, strlen($matchedCustomRoute) + 1);
+                        $paramValues = explode('/', $paramsUri);
+                        $params = array();
+                        foreach($routeParamNames as $paramName) {
+                            $params[$paramName] = array_shift($paramValues);
+                        }
+
+                        $input->setGet($params);
                     }
-                }
-            } else {
-                throw new \Exception('Main route missing', 500);
-            }
 
-            if ($this->namespace == null && $routes['*']['namespace']) {
-                $this->namespace = $routes['*']['namespace'];
-                $routeController=$routes['*'];
-            } else if ($this->namespace == null && !$routes['*']['namespace']) {
-                throw new \Exception('Main route missing', 500);
-            }
-            $input = \My\Mvc\Core\InputData::getInstance();
-            $params = explode('/', $_uri);
-            if ($params[0]) {
-                $this->controller= strtolower($params[0]);
-                //if we do not have controller and method, we do not have params
-                if ($params[1]) {
-                    $this->method=strtolower($params[1]);
-                    unset($params[0],$params[1]);
-                    $input->setGet(array_values($params));
-                } else {
-                    $this->method=$this->getDefaultMethod();
-                }
-            } else {
-                $this->controller= $this->getDefaultController();
-                $this->method=$this->getDefaultMethod();
-            }
 
-            if(is_array($routeController) && $routeController['controllers']){
-                if($routeController['controllers'][$this->controller]['methods'][$this->method]){
-                    $this->method = strtolower($routeController['controllers'][$this->controller]['methods'][$this->method]);
-                }
-                if(isset($routeController['controllers'][$this->controller]['to'])){
-                    $this->controller = strtolower($routeController['controllers'][$this->controller]['to']);
+                    if($hasParams) {
+                        $matchedCustomRoute = substr($matchedCustomRoute, strlen($customRoute) + 1);
+                    } else {
+                        $matchedCustomRoute = substr($uri, strlen($customRoute) + 1);
+                    }
+
+                    $matchedCustomRouteArray = explode('/', $matchedCustomRoute);
+
+                    if($controllerAction[1]) {
+                        $this->controller = $controllerAction[1];
+                    } else {
+                        $this->controller = array_shift($matchedCustomRouteArray);
+                        if(!$this->controller) {
+                            $this->controller = $this->getDefaultController();
+                        }
+                    }
+                    if($controllerAction[2]) {
+                        $this->method = $controllerAction[2];
+                    } else {
+                        $this->method = array_shift($matchedCustomRouteArray);
+                        if(!$this->method) {
+                            $this->method = $this->getDefaultMethod();
+                        }
+                    }
+                    $input->setPost($this->router->getPost());
+                    $this->namespace = $controllerAction[0] ? $controllerAction[0] : $this->config->app['defaultControllerNamespace'];
+                    $this->loadControllerAction();
+                    return true;
                 }
             }
-
-            $input->setPost($this->router->getPost());
-            //TODO fix it
-            $fullQualifiedName = $this->namespace . '\\' . ucfirst($this->controller) . $this->config->app['controllersSuffix'];
-            $newControoler = new $fullQualifiedName();
-            $newControoler->{$this->method}();
         }
-
-        public function getDefaultController() {
-            $controler = \My\Mvc\Core\Application::getInstance()->getConfig()->app['default_controller'];
-            if ($controler) {
-                return strtolower($controler);
-            }
-            return 'index';
-        }
-
-        public function getDefaultMethod() {
-            $method = \My\Mvc\Core\Application::getInstance()->getConfig()->app['default_method'];
-            if ($method) {
-                return strtolower($method);
-            }
-            return 'index';
-        }
-
-        /**
-         * @return \My\Mvc\Core\FrontController
-         */
-        public static function getInstance() {
-            if (self::$_instance == null) {
-                self::$_instance = new \My\Mvc\Core\FrontController();
-            }
-            return self::$_instance;
-        }
-
     }
 }
---
+++ ShoppingCart/Models/BindingModels/User/LoginUser.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peter
+ * Date: 10/3/2015
+ * Time: 8:14 AM
+ */
+
+namespace My\ShoppingCart\Models\BindingModels\User;
+
+
+class LoginUser
+{
+
+}
